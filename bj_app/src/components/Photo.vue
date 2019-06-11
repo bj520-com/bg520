9605494a652e313ebe7b5fb6a2cd912cd346adf1
EternalHouri <EternalHouri@163.com>
2019-06-11 20:40:17 +0800

    photo

--- bj_app/src/components/Photo.vue
+++ bj_app/src/components/Photo.vue
@@ -4,11 +4,11 @@
       <!-- <el-col :span="12" :offset="index > 0 ? 2 : 0"> -->
         <el-card :body-style="{ padding: '0px' }" class="img-father">
           <a href="#">
-            <!-- <img :src="imgUrl" class="image"> -->
-            <img src="cuspho/mayfourth/1.jpg" class="image">
+            <img :src="imgUrl" class="image">
+            <!-- <img src="cuspho/mayfourth/1.jpg" class="image"> -->
             <div style="padding: 14px;" class="title-father">
-              <!-- <span>{{imgTitle}}</span> -->
-              <span class="img-title">5月第4季最新客照</span>
+              <span class="img-title">{{imgTitle}}</span>
+              <!-- <span class="img-title">5月第4季最新客照</span> -->
               <i class="el-icon-right"></i>
             </div>
           </a>
@@ -16,13 +16,19 @@
         </el-card>
       <!-- </el-col> -->
     </el-row>
-
+    
   </div>
 </template>
 
 <script>
   export default {
-    
+    data(){
+      return{}
+    },
+    props:{
+      imgUrl:{default:""},
+      imgTitle:{default:""}
+    }
   }
 </script>
 
@@ -30,21 +36,42 @@
 .img-father{
   width: 33%;
   margin-top: 36px;
+  transition: all .3s ease-out 0s;
+
+  .image{
+    width: 99%;
+    height: auto;
+    transition: all .3s ease-out 0s;
+  }
+
+  .image:hover{
+    border:2px solid #e7090b;
+  }
+  .title-father{line-height: 0px;}
+  .img-title{
+    color: #666;
+    font-size: 16px;
+  }
+
+  .el-icon-right{
+    margin-left: 140px;
+    font-size: 20px;
+    color: #ddd;
+    padding: 5px;
+  }
 }
-.image{
-  width: 100%;
-  height: auto;
-}
-.image:hover{border:3px solid }
-.title-father{line-height: 0px;}
-.img-title{
-  color: #666;
-  font-size: 16px;
+.img-father:hover{
+  box-shadow: 0 0 18px #666;
+  transform: scale(1.01);
+  .el-icon-right{
+    background: #e7090b;
+    border-radius: 50%;
+    // color: #fff;
+  }
 }
 
-.el-icon-right{
-  margin-left: 140px;
-  font-size: 20px;
-  color: #ddd;
-}
+
+
+
+
 </style>
--- bj_app/src/views/photoList.vue
+++ bj_app/src/views/photoList.vue
@@ -4,19 +4,22 @@
       <!-- 顶部面包屑导航 -->
       <div class="t-breadcrumbs">
         <el-breadcrumb separator-class="el-icon-arrow-right">
-        <el-breadcrumb-item>
-          您当前位置：
-          <a href="Index">铂爵(伯爵)旅拍婚纱摄影</a>
-        </el-breadcrumb-item>
-        <el-breadcrumb-item>
-          <a href="Index">客户婚纱照</a>
-        </el-breadcrumb-item>
-      </el-breadcrumb>
+          <el-breadcrumb-item>
+            您当前位置：
+            <a href="Index">铂爵(伯爵)旅拍婚纱摄影</a>
+          </el-breadcrumb-item>
+          <el-breadcrumb-item>
+            <a href="Index">客户婚纱照</a>
+          </el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
       <!-- 图片列表 -->
       <div class="">
-        <my-photo></my-photo>
+        <my-photo imgUrl="cuspho/mayfourth/1.jpg" imgTitle="5月第4季最新客照"></my-photo>
       </div>
-      </div>
+      <!-- 底部分页 -->
+      <el-pagination :page-size="9" :pager-count="6" layout="prev, pager, next" :total="90" :prev-text="lpage" :next-text="rpage" >
+      </el-pagination>
     </div>
   </div>
 </template>
@@ -25,7 +28,10 @@
 import MyPhoto from '../components/Photo.vue'
   export default {
     data(){
-      return {}
+      return {
+        lpage:"上一页",
+        rpage:"下一页"
+      }
     },
     components:{
       MyPhoto
@@ -35,26 +41,32 @@ import MyPhoto from '../components/Photo.vue'
 
 <style lang="scss" scoped>
   .container {
-  width: 70%;
-  margin: auto;
-  // 顶部面包屑
-  .t-breadcrumbs {
-    margin-top: 36px;
-    font-family: 宋体;
+    width: 70%;
+    margin: auto;
+    // 顶部面包屑
+    .t-breadcrumbs {
+      margin-top: 36px;
+      font-family: 宋体;
 
-    .el-breadcrumb {
-      .el-breadcrumb__item {
-        a,
-        span {
-          font-size: 0.15rem;
-          font-weight: 500;
+      .el-breadcrumb {
+        .el-breadcrumb__item {
+          a,
+          span {
+            font-size: 0.15rem;
+            font-weight: 500;
+          }
         }
       }
-    }
 
-    a:hover {
-      color: #f00;
+      a:hover {
+        color: #f00;
+      }
     }
+    // 底部分页
+    div.el-pagination{ 
+      margin-top: 36px;
+      color: #aaa;
+    }
+    
   }
-}
 </style>
